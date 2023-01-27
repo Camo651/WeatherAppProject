8aebcd1510662a541ed19dda2cf9bb98033d441a
Camo <47070087+Camo651@users.noreply.github.com>
2023-01-27 14:47:40 -0500

    Updated from FTP

--- weatherApp.js
+++ weatherApp.js
@@ -1,12 +1,34 @@
-var currentTime = new Date();
-var secondsSinceMidnight = currentTime.getHours() * 3600 + currentTime.getMinutes() * 60 + currentTime.getSeconds();
+var day_clearSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M23.25 10.6V4.1h1.5v6.5ZM34 15.1l-1.05-1.05L37.5 9.3l1.15 1.15Zm3.4 9.65v-1.5h6.5v1.5ZM23.25 43.9v-6.5h1.5v6.5Zm-9.2-28.85L9.3 10.5l1.2-1.1 4.65 4.6Zm23.5 23.65-4.6-4.75 1.05-1 4.6 4.55ZM4.1 24.75v-1.5h6.5v1.5Zm6.35 13.95L9.4 37.5l4.55-4.55.55.55.6.55ZM24 34q-4.15 0-7.075-2.925T14 24q0-4.15 2.925-7.075T24 14q4.15 0 7.075 2.925T34 24q0 4.15-2.925 7.075T24 34Z"/></svg>';
+var day_snowSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M23.25 43V32.65l-7.95 7.9-1.15-1.1 9.1-9.05v-5.65H17.6l-8.95 9-1.15-1.1 7.85-7.9H5v-1.5h10.35l-7.9-8 1.1-1.1 9.05 9.1h5.65V17.6l-9-8.95 1.1-1.15 7.9 7.8V5h1.5v10.3l8-7.85 1.1 1.1-9.1 9.05v5.65h5.65l9-9 1.1 1.1-7.8 7.9H43v1.5H32.7l7.85 7.95-1.1 1.15-9.05-9.1h-5.65v5.65l9.1 9.05-1.1 1.1-8-7.9V43Z"/></svg>';
+var day_thunderSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m25.6 47 4.2-4.75-4-2L30.35 35h2.05l-4.2 4.75 4 2L27.65 47Zm-12 0 4.2-4.75-4-2L18.35 35h2.05l-4.2 4.75 4 2L15.65 47Zm1.15-16.45q-4 0-6.875-2.875T5 20.75q0-3.8 2.625-6.6T14.3 11q1.5-2.8 4-4.4Q20.8 5 24 5q4.3 0 7.3 2.85t3.5 7.1q3.95.15 6.075 2.475T43 22.75q0 3.25-2.275 5.525-2.275 2.275-5.475 2.275Z"/></svg>';
+var day_rainSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.65 43.25q-.3.15-.625.05-.325-.1-.425-.4L23 35.7q-.15-.25-.05-.575.1-.325.35-.475.3-.1.625 0t.475.4L28 42.2q.15.25.025.575-.125.325-.375.475Zm12-.05q-.25.15-.575.05-.325-.1-.475-.4L35 35.7q-.1-.3 0-.6t.35-.45q.3-.15.6-.05.3.1.5.4l3.6 7.15q.15.3.025.625t-.425.425Zm-24 0q-.3.15-.625.075t-.475-.375l-3.6-7.2q-.15-.25-.025-.575.125-.325.425-.475.25-.1.575-.025.325.075.475.375l3.6 7.2q.1.25 0 .575-.1.325-.35.425Zm-.9-12.65q-4 0-6.875-2.875T5 20.75q0-3.8 2.625-6.6T14.3 11q1.5-2.8 4-4.4Q20.8 5 24 5q4.3 0 7.3 2.85t3.5 7.1q3.95.15 6.075 2.475T43 22.75q0 3.25-2.275 5.525-2.275 2.275-5.475 2.275Z"/></svg>';
+var day_partlyCloudySVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M13.15 37.6q-2.65 0-4.525-1.85-1.875-1.85-1.875-4.5 0-2.5 1.775-4.25t4.225-1.75q1.75 0 3.15.9t2.15 2.45l.9 2.25h2.5q1.4 0 2.35 1 .95 1 .95 2.4t-.975 2.375q-.975.975-2.375.975Zm15-4.5q-.5-2.55-2.375-4.225Q23.9 27.2 21.35 27.35q-.8-2.3-2.725-3.8t-4.375-1.7q.7-3.45 3.45-5.65T24 14q4.15 0 7.075 2.925T34 24q0 3-1.6 5.425T28.15 33.1Zm-4.9-22.5V4.1h1.5v6.5ZM34 15.1l-1.1-1.15 4.65-4.65 1.1 1.15Zm3.4 9.65v-1.5h6.5v1.5Zm.15 13.9L32.95 34l1.15-1.15 4.6 4.65Zm-23.5-23.5L9.3 10.4l1.15-1 4.7 4.65Z"/></svg>';
+var day_cloudySVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M12.8 38q-3.7 0-6.25-2.575T4 29.2q0-3.45 2.425-6.075T12.35 20.4q.55-4.45 3.875-7.425Q19.55 10 24.05 10q4.9 0 8.275 3.5t3.375 8.45v2.55h1.25q2.95-.1 5 1.825T44 31.25q0 2.8-1.975 4.775Q40.05 38 37.25 38Z"/></svg>';
+
+var night_clearSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M26.2 44q-3.8 0-7.1-1.425t-5.775-3.9Q10.85 36.2 9.425 32.9 8 29.6 8 25.8q0-5.85 3.35-10.525 3.35-4.675 8.9-6.525-.25 4.65 1.4 8.975 1.65 4.325 5 7.625 3.3 3.35 7.625 5t8.975 1.4q-1.8 5.55-6.5 8.9Q32.05 44 26.2 44Z"/></svg>';
+var night_snowSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M23.25 43V32.65l-7.95 7.9-1.15-1.1 9.1-9.05v-5.65H17.6l-8.95 9-1.15-1.1 7.85-7.9H5v-1.5h10.35l-7.9-8 1.1-1.1 9.05 9.1h5.65V17.6l-9-8.95 1.1-1.15 7.9 7.8V5h1.5v10.3l8-7.85 1.1 1.1-9.1 9.05v5.65h5.65l9-9 1.1 1.1-7.8 7.9H43v1.5H32.7l7.85 7.95-1.1 1.15-9.05-9.1h-5.65v5.65l9.1 9.05-1.1 1.1-8-7.9V43Z"/></svg>';
+var night_thunderSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m25.6 47 4.2-4.75-4-2L30.35 35h2.05l-4.2 4.75 4 2L27.65 47Zm-12 0 4.2-4.75-4-2L18.35 35h2.05l-4.2 4.75 4 2L15.65 47Zm1.15-16.45q-4 0-6.875-2.875T5 20.75q0-3.8 2.625-6.6T14.3 11q1.5-2.8 4-4.4Q20.8 5 24 5q4.3 0 7.3 2.85t3.5 7.1q3.95.15 6.075 2.475T43 22.75q0 3.25-2.275 5.525-2.275 2.275-5.475 2.275Z"/></svg>';
+var night_rainSVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.65 43.25q-.3.15-.625.05-.325-.1-.425-.4L23 35.7q-.15-.25-.05-.575.1-.325.35-.475.3-.1.625 0t.475.4L28 42.2q.15.25.025.575-.125.325-.375.475Zm12-.05q-.25.15-.575.05-.325-.1-.475-.4L35 35.7q-.1-.3 0-.6t.35-.45q.3-.15.6-.05.3.1.5.4l3.6 7.15q.15.3.025.625t-.425.425Zm-24 0q-.3.15-.625.075t-.475-.375l-3.6-7.2q-.15-.25-.025-.575.125-.325.425-.475.25-.1.575-.025.325.075.475.375l3.6 7.2q.1.25 0 .575-.1.325-.35.425Zm-.9-12.65q-4 0-6.875-2.875T5 20.75q0-3.8 2.625-6.6T14.3 11q1.5-2.8 4-4.4Q20.8 5 24 5q4.3 0 7.3 2.85t3.5 7.1q3.95.15 6.075 2.475T43 22.75q0 3.25-2.275 5.525-2.275 2.275-5.475 2.275Z"/></svg>';
+var night_partlyCloudySVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M12 29.3q2 0 3.675 1.1 1.675 1.1 2.475 2.95l.8 1.95h2.15q1.5 0 2.55 1.075Q24.7 37.45 24.7 39t-1.1 2.625Q22.5 42.7 21 42.7h-9q-2.75 0-4.725-1.975Q5.3 38.75 5.3 36q0-2.8 1.975-4.75Q9.25 29.3 12 29.3Zm8.25-20.55q-.25 4.65 1.4 8.975 1.65 4.325 5 7.625 3.3 3.35 7.625 5t8.975 1.4q-1.8 5.55-6.475 8.875T26.3 44h-.2q1.05-.95 1.6-2.25t.55-2.75q0-2.95-2.025-5.05-2.025-2.1-4.875-2.1-1.25-2.8-3.775-4.45Q15.05 25.75 12 25.75q-1 0-2.025.225T8 26.65v-.8Q8 20 11.35 15.3q3.35-4.7 8.9-6.55Z"/></svg>';
+var night_cloudySVG = '<svg id="wcSVG" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M12.8 38q-3.7 0-6.25-2.575T4 29.2q0-3.45 2.425-6.075T12.35 20.4q.55-4.45 3.875-7.425Q19.55 10 24.05 10q4.9 0 8.275 3.5t3.375 8.45v2.55h1.25q2.95-.1 5 1.825T44 31.25q0 2.8-1.975 4.775Q40.05 38 37.25 38Z"/></svg>';
+
+var secondsSinceMidnight = secondsSinceMidnightDate(new Date());
 var sunriseTime = 25;
 var sunsetTime = 75;
 
+var isDay = secondsSinceMidnight > sunriseTime && secondsSinceMidnight < sunsetTime;
+
+function secondsSinceMidnightDate(date){
+    return date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+}
+
 function getSunGradient(){
-    var day = [12,101,255];
+    var day = [102,142,213];
     var night = [24,5,55];
     var sun = [193,132,53];
+    // var day = [0,0,255];
+    // var night = [255,0,0];
+    // var sun = [0,255,0];
     var sunTime = 3
     var sunGradient = {};
     sunGradient[0] = night;
@@ -18,19 +40,21 @@ function getSunGradient(){
     sunGradient[sunsetTime] = sun;
     sunGradient[sunsetTime + sunTime] = night;
     sunGradient[100] = night;
+
+    return sunGradient;
 }
 
-var aaa = 0;
-animate();
-async function animate() {
+function seBackground() {
     var currentTime = new Date();
-    // var secondsSinceMidnight = currentTime.getHours() * 3600 + currentTime.getMinutes() * 60 + currentTime.getSeconds();
-    // var timeNormalized = secondsSinceMidnight / 86400;
-    var timeNormalized = aaa / 1000;
-    aaa += 1;
+    var secondsSinceMidnight = currentTime.getHours() * 3600 + currentTime.getMinutes() * 60 + currentTime.getSeconds();
+    var timeNormalized = secondsSinceMidnight / 86400;
     var currentColor = evaluateGradient(getSunGradient(), timeNormalized);
-    document.body.style.backgroundColor = colorToString(currentColor);
-    requestAnimationFrame(animate);
+    var currentColorLightened = colorLighten(currentColor, .35);
+    var skyGradient = "linear-gradient(30deg, "+colorToString(currentColorLightened)+" 0%, "+colorToString(currentColor)+" 100%)";
+    document.body.style.background = skyGradient+" no-repeat center center fixed";
+    var darkColor = colorToString(colorDarken(currentColor, .7));
+    document.getElementById("mtnPath").setAttribute("fill", darkColor);
+    document.getElementById("forecast").setAttribute("style", "background-color: "+darkColor);
 }
 
 getLocation();
@@ -64,21 +88,28 @@ function error() {
 function setData(data) {
     var weatherData = JSON.parse(data);
     console.log("weatherData: " + JSON.stringify(weatherData, null, "\t"));
-
     var currentTemp = celciusToFahrenheit(weatherData.current_weather.temperature);
     var currentWeatherCode = weatherData.current_weather.weathercode;
-    document.getElementById("currentTempTemp").innerHTML = currentTemp + "°F";
-    document.getElementById("currentTempText").innerHTML = weatherCodeToTitle(currentWeatherCode);
+    document.getElementById("currentTempTemp").innerHTML = currentTemp + "°";
+    document.getElementById("currentTempText").innerHTML = weatherCodeToSVG(currentWeatherCode, isDay) + weatherCodeToTitle(currentWeatherCode);
 
-    sunriseTime = weatherData.daily[0].sunrise;
-    console.log("sunriseTime: " + sunriseTime);
-    sunsetTime = weatherData.daily[0].sunset;
+    var forecast = weatherData.daily;
+    for(var i = 0; i < 5; i++){
+        var dayTemp = celciusToFahrenheit(forecast.temperature_2m_max[i]);
+        var nightTemp = celciusToFahrenheit(forecast.temperature_2m_min[i]);
+        var weatherCode = forecast.weathercode[i];
+        var date = new Date(forecast.time[i]);
+        var dayTitle = (i==0)?"Today":["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()];// + " " + (date.getMonth() + 1) + "/" + date.getDate();
+        document.getElementById("dayTitle"+i).innerHTML = weatherCodeToSVG(weatherCode, true) + dayTitle;
+        document.getElementById("dayTemp"+i).innerHTML = dayTemp + "°" + " / " + nightTemp + "°" + "<br>" + weatherCodeToTitle(weatherCode);
+    }
 
+    sunriseTime = Math.floor(secondsSinceMidnightDate(new Date(weatherData.daily.sunrise[0])) / 86400 * 100);
+    sunsetTime = Math.floor(secondsSinceMidnightDate(new Date(weatherData.daily.sunset[0])) / 86400 * 100);
+    seBackground();
 }
 
-function celciusToFahrenheit(celcius) {
-    return Math.round(celcius * 9/5 + 32);
-}
+function celciusToFahrenheit(celcius) {return Math.round(celcius * 9/5 + 32);}
 
 function weatherCodeToTitle(code){
     var weatherCodes = { 0: "Clear sky", 1: "Mainly clear", 2: "Partly cloudy", 3: "Overcast", 45: "Fog", 48: "Fog", 51: "Light drizzle", 53: "Moderate drizzle", 55: "Heavy drizzle", 56: "Light freezing drizzle", 57: "Heavy freezing drizzle", 61: "Slight rain", 63: "Moderate rain", 65: "Heavy rain", 66: "Light freezing rain", 67: "Heavy freezing rain", 71: "Light snow", 73: "Moderate snow", 75: "Heavy snow", 77: "Snow grains", 80: "Light rain showers", 81: "Moderate rain showers", 82: "Violent rain showers", 85: "Light snow showers", 86: "Heavy snow showers", 95: "Light thunderstorm", 96: "Moderate thunderstorm", 99: "Heavy thunderstorm"};
@@ -96,7 +127,39 @@ async function setLocationDisplay(lat, lon){
     }
 }
 
-
+function weatherCodeToSVG(code, isDay){
+    switch(code){
+        case 0: return isDay?day_clearSVG:night_clearSVG; //clear
+        case 1: return isDay?day_clearSVG:night_clearSVG; //mainly clear
+        case 2: return isDay?day_partlyCloudySVG:night_partlyCloudySVG; //partly cloudy
+        case 3: return isDay?day_cloudySVG:night_cloudySVG; //overcast
+        case 45: return isDay?day_cloudySVG:night_cloudySVG; //fog
+        case 48: return isDay?day_cloudySVG:night_cloudySVG; //fog
+        case 51: return isDay?day_rainSVG:night_rainSVG; //light drizzle
+        case 53: return isDay?day_rainSVG:night_rainSVG; //moderate drizzle
+        case 55: return isDay?day_rainSVG:night_rainSVG; //heavy drizzle
+        case 56: return isDay?day_rainSVG:night_rainSVG; //light freezing drizzle
+        case 57: return isDay?day_rainSVG:night_rainSVG; //heavy freezing drizzle
+        case 61: return isDay?day_rainSVG:night_rainSVG; //slight rain
+        case 63: return isDay?day_rainSVG:night_rainSVG; //moderate rain
+        case 65: return isDay?day_rainSVG:night_rainSVG; //heavy rain
+        case 66: return isDay?day_rainSVG:night_rainSVG; //light freezing rain
+        case 67: return isDay?day_rainSVG:night_rainSVG; //heavy freezing rain
+        case 71: return isDay?day_snowSVG:night_snowSVG; //light snow
+        case 73: return isDay?day_snowSVG:night_snowSVG; //moderate snow
+        case 75: return isDay?day_snowSVG:night_snowSVG; //heavy snow
+        case 77: return isDay?day_snowSVG:night_snowSVG; //snow grains
+        case 80: return isDay?day_rainSVG:night_rainSVG; //light rain showers
+        case 81: return isDay?day_rainSVG:night_rainSVG; //moderate rain showers
+        case 82: return isDay?day_rainSVG:night_rainSVG; //violent rain showers
+        case 85: return isDay?day_snowSVG:night_snowSVG; //light snow showers
+        case 86: return isDay?day_snowSVG:night_snowSVG; //heavy snow showers
+        case 95: return isDay?day_thunderSVG:night_thunderSVG; //light thunderstorm
+        case 96: return isDay?day_thunderSVG:night_thunderSVG; //moderate thunderstorm
+        case 99: return isDay?day_thunderSVG:night_thunderSVG; //heavy thunderstorm
+        default: return isDay?day_clearSVG:night_clearSVG;
+    }
+}
 
 function evaluateGradient(g, t) {
     t = clamp(t*100, 0, 100);
@@ -112,9 +175,12 @@ function evaluateGradient(g, t) {
     }
     var lowerColor = g[lowerKey];
     var upperColor = g[upperKey];
+    console.log("lowerKey: " + lowerKey + ", upperKey: " + upperKey + ", lowerColor: " + lowerColor + ", upperColor: " + upperColor);
     var tNormalized = (t - lowerKey) / (upperKey - lowerKey);
     return interpolateColor(lowerColor, upperColor, tNormalized);
 }
+function colorLighten(c, t) {return interpolateColor([255, 255, 255], c, t);}
+function colorDarken(c, t) {return interpolateColor([0, 0, 0], c, t);}
 function colorToString(c) {return "rgb(" + c[0] + "," + c[1] + "," + c[2] + ")";}
 function interpolateColor(c1, c2, t) {return [Math.floor(lerp(c1[0], c2[0], t)),Math.floor(lerp(c1[1], c2[1], t)),Math.floor(lerp(c1[2], c2[2], t))]}
 function lerp(a, b, t) {return a + (b - a) * clamp(t, 0, 1);}
